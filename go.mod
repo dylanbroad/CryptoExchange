76ce71d6a06894f74700d79d03e59b8200d27b40
Dylan Broad <dylanbroad12@gmail.com>
2024-03-19 19:41:40 -0400

    First commit

---
+++ go.mod
@@ -0,0 +1,14 @@
+module go-project
+
+go 1.22.1
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/gorilla/websocket v1.5.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	golang.org/x/crypto v0.21.0 // indirect
+	golang.org/x/net v0.22.0 // indirect
+	golang.org/x/oauth2 v0.18.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
---
+++ pkg/coinbase/websocket.go
@@ -0,0 +1,86 @@
+package websocket
+
+import (
+    "log"
+    "os"
+    "os/signal"
+    "syscall"
+	"time"
+	
+
+    "github.com/gorilla/websocket"
+)
+
+func main() {
+	//Handle the interrupt signal
+    interrupt := make(chan os.Signal, 1)
+    signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+
+	//Initialize the websocket connection
+    var addr = "ws-feed-public.sandbox.exchange.coinbase.com"
+    c, _, err := websocket.DefaultDialer.Dial("wss://"+addr, nil)
+    if err != nil {
+        log.Fatal("dial:", err)
+    }
+    defer c.Close()
+
+    done := make(chan struct{})
+
+    go func() {
+        defer close(done)
+        for {
+            _, message, err := c.ReadMessage()
+            if err != nil {
+                log.Println("read:", err)
+                return
+            }
+            log.Printf("recv: %s", message)
+        }
+    }()
+
+    // Subscribe to ETH-USD and ETH-EUR with the level2, heartbeat, and ticker channels
+    err = c.WriteMessage(websocket.TextMessage, []byte(`{
+        "type": "subscribe",
+        "product_ids": [
+            "ETH-USD",
+            "ETH-EUR"
+        ],
+        "channels": [
+            "level2",
+            "heartbeat",
+            {
+                "name": "ticker",
+                "product_ids": [
+                    "ETH-BTC",
+                    "ETH-USD"
+                ]
+            }
+        ]
+    }`))
+    if err != nil {
+        log.Fatal("write:", err)
+    }
+
+    for {
+        select {
+        case <-done:
+            return
+        case <-interrupt:
+            log.Println("interrupt")
+            // Cleanly close the connection by sending a close message and then
+            // waiting (with timeout) for the server to close the connection.
+            err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+            if err != nil {
+                log.Println("write close:", err)
+                return
+            }
+            select {
+            case <-done:
+            case <-time.After(time.Second):
+            }
+            c.Close()
+            return
+        }
+    }
+}
